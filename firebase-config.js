01fe19c0d7170186531783732352e6ab58d047e4
gokhanlones <79420556+gokhanlones@users.noreply.github.com>
2026-07-08 00:08:44 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCIoKO6kddlTDN6ekHf0Ct2LDjrLF9rXo8",
+    authDomain: "tarotistan-a6143.firebaseapp.com",
+    projectId: "tarotistan-a6143",
+    storageBucket: "tarotistan-a6143.firebasestorage.app",
+    messagingSenderId: "201197341458",
+    appId: "1:201197341458:web:ff4a6553f819c5a39281cb"
+};
+
+firebase.initializeApp(firebaseConfig);
